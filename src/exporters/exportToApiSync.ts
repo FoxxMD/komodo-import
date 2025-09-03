c05ee3183c0c09d475addee9f021750f904ce748
FoxxMD <FoxxMD@users.noreply.github.com>
2025-09-03 14:06:18 +0000

    feat: Parse komodo api error response to proper Error
    * Add logging before api actions for easier debugging
    * Type gaurd komodo api response and implement proper Error to parse result
      * Include komodo trace at end of stack
      * Makes respones actually show up in logging
    * try-catch write operations for more descriptive stack

--- src/exporters/exportToApiSync.ts
+++ src/exporters/exportToApiSync.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import { stripIndents } from "common-tags";
 import dayjs from "dayjs";
 import { getDefaultKomodoApi } from "../common/utils/komodo.js";
+import { asKomodoApiErrorResponse, KomodoApiError } from "../common/errors.js";
 
 export const exportToSync = async (toml: string, parentLogger: Logger): Promise<void> => {
     const logger = childLogger(parentLogger, 'Sync API');
@@ -12,8 +13,8 @@ export const exportToSync = async (toml: string, parentLogger: Logger): Promise<
     if (parseBool(process.env.OUTPUT_API_SYNC)) {
         try {
             const komodo = getDefaultKomodoApi();
-            if(komodo === undefined) {
-                throw new Error('Komodo API is unavaiable, cannot export to Sync');
+            if (komodo === undefined) {
+                throw new Error('Komodo API is unavailable, cannot export to Sync');
             }
             const syncName = isUndefinedOrEmptyString(process.env.SYNC_NAME) ? 'komodo-import' : process.env.SYNC_NAME;
             logger.info(`Using '${syncName}' as Sync Name`);
@@ -41,46 +42,68 @@ export const exportToSync = async (toml: string, parentLogger: Logger): Promise<
                 }
                 logger.verbose(`Resource Sync ${syncName} alerady exists (ID ${syncId}), will ${existingBehavior} contents`);
             } catch (e) {
-                // do nothing
+                if (asKomodoApiErrorResponse(e)) {
+                    if (e.result.error.toLocaleLowerCase().includes('did not find any resourcesync matching')) {
+                        logger.verbose(`No Sync named ${syncName} exists, creating a new one.`);
+                    } else {
+                        const ke = new KomodoApiError(e);
+                        throw new Error('Unexpected error from Komodo API while checking for existing Sync', { cause: ke });
+                    }
+                } else {
+                    throw new Error('Unexpected error while trying to check for existing Sync', { cause: e });
+                }
             }
 
 
-            if (syncId === undefined) {
-                const sync = await komodo.api.write('CreateResourceSync', {
-                    name: syncName,
-                    config: {
-                        include_resources: true,
-                        file_contents: toml
-                    }
-                });
-                syncId = sync._id.$oid;
-                logger.info('Resource Sync created.');
-            } else {
-                if (existingBehavior === 'overwrite') {
-                    const sync = await komodo.api.write('UpdateResourceSync', {
-                        id: syncId,
+            try {
+
+
+                if (syncId === undefined) {
+                    logger.debug('Trying to create Sync via API...');
+                    const sync = await komodo.api.write('CreateResourceSync', {
+                        name: syncName,
                         config: {
                             include_resources: true,
                             file_contents: toml
                         }
                     });
-                     logger.info('Resource Sync overwrriten.');
+                    syncId = sync._id.$oid;
+                    logger.info('Resource Sync created.');
                 } else {
-                    const time = dayjs().format('YYYY-MM-DD--HH-mm-ss');
-                    const sync = await komodo.api.write('UpdateResourceSync', {
-                        id: syncId,
-                        config: {
-                            include_resources: true,
-                            file_contents: stripIndents`${existingSync.config.file_contents}
+                    if (existingBehavior === 'overwrite') {
+                        logger.debug('Trying to overwrite existing Sync via API...');
+                        const sync = await komodo.api.write('UpdateResourceSync', {
+                            id: syncId,
+                            config: {
+                                include_resources: true,
+                                file_contents: toml
+                            }
+                        });
+                        logger.info('Resource Sync overwritten.');
+                    } else {
+                        logger.debug('Trying to append to existing Sync via API...');
+                        const time = dayjs().format('YYYY-MM-DD--HH-mm-ss');
+                        const sync = await komodo.api.write('UpdateResourceSync', {
+                            id: syncId,
+                            config: {
+                                include_resources: true,
+                                file_contents: stripIndents`${existingSync.config.file_contents}
                             
                             ## Added by Komodo Import ${time}
                             
                             ${toml}
                             
                             ##`
-                        }
-                    });
-                     logger.info('Resource Sync appended.');
+                            }
+                        });
+                        logger.info('Resource Sync appended.');
+                    }
+                }
+            } catch (e) {
+                if (asKomodoApiErrorResponse(e)) {
+                    throw new Error('Unexpected error from Komodo API while writing Sync', { cause: new KomodoApiError(e) });
+                } else {
+                    throw new Error('Unexpected error while trying to write to Sync', { cause: e });
                 }
             }
 
@@ -89,14 +112,7 @@ export const exportToSync = async (toml: string, parentLogger: Logger): Promise<
             logger.info(`Resource Sync URL: ${syncUrl}`);
         }
         catch (e) {
-            let hint: string;
-            switch (e.result?.error) {
-                case 'Must provide unique name for resource.':
-                    hint = 'Sync already exists! Use a different name.'
-                    break;
-            }
-            logger.error(e);
-            throw new Error(`Komodo API error occurred while trying to export to Resource Sync${hint !== undefined ? `. Hint: ${hint}` : ''}`, { cause: e });
+            throw new Error(`Error occurred while trying to export to Resource Sync`, { cause: e });
         }
     } else if (isDebugMode()) {
         logger.debug('Not exporting to Resource Sync because env KOMODO_URL was not defined.');
